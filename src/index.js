ca5b698fb3e7e7d17018fb5ce649742876d7dc49
korolkovskreiss <113632378+korolkovskreiss@users.noreply.github.com>
2023-02-19 01:08:51 +0200

    Init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+	let openBrackets = []
+	let braсketsMap = {}
+  bracketsConfig.forEach( value => {
+  	openBrackets.push(value[0]);
+  	braсketsMap[value[1]] = value[0];
+  })
+ 	for ( let i = 0; i < str.length; i++){
+  	let currentBracket = str[i];
+     if (currentBracket === stack[stack.length - 1] && braсketsMap[currentBracket] === currentBracket ){
+      stack.pop()
+   }
+    else if(openBrackets.includes(currentBracket)){
+    	stack.push(currentBracket)
+    } else {
+    	if (stack.length === 0) {
+      	return false
+      }
+      let stackTop = stack[stack.length - 1]
+      if (braсketsMap[currentBracket] === stackTop ){
+      	stack.pop()
+      } else {
+      	return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
